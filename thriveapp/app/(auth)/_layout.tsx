d2c414fb8c7a20025ad37b2910affe403df9f58b
jackparsonsuk <jwjparsons@gmail.com>
2026-03-10 11:29:37 +0000

    Reset password header

--- thriveapp/app/(auth)/_layout.tsx
+++ thriveapp/app/(auth)/_layout.tsx
@@ -5,6 +5,8 @@ export default function AuthLayout() {
         <Stack>
             <Stack.Screen name="login" options={{ headerShown: false, title: 'Sign In' }} />
             <Stack.Screen name="signup" options={{ headerShown: false, title: 'Create Account' }} />
+            <Stack.Screen name="forgot-password" options={{ headerShown: false, title: 'Reset Password' }} />
+            <Stack.Screen name="forgot-password-success" options={{ headerShown: false, title: 'Email Sent' }} />
         </Stack>
     );
 }
